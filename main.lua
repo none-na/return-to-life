443812dbd716c3b38d98467d1c557061eb06a9a9
Assa <cytokininwastaken@gmail.com>
2020-10-11 15:10:00 +0200

    disabled item loader for a bit

--- main.lua
+++ main.lua
@@ -4,4 +4,4 @@ require("restre")()
 
 restre_require("misc/title_screen/title_screen")
 restre_require("misc/teleporter_effects/teleporter_effects")
-restre_require("Items/itemLoader")
+--restre_require("Items/itemLoader")
